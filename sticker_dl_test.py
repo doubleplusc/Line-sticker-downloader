4997564c39ea8a0363921390b04003c3419d3d71
doubleplusc <changli@usc.edu>
2016-01-18 23:18:01 -0800

    From the test file, strange things happen when a mix of Unicode and escape sequences are in the parsed name. Definitely need the regexp from the SO post...
    Better hope the user has Unicode... only IDLE displays print statements
    correctly. bash.exe and Windows console will not. (When your print debug
    statements all give differing answers... which one can you trust?!?)
    
    Fixed array index that caused extra " to appear in beginning of pack_name.

--- sticker_dl_test.py
+++ sticker_dl_test.py
@@ -5,10 +5,10 @@ def main():
     # http://stackoverflow.com/questions/4020539/process-escape-sequences-in-a-string-in-python
     # 2 hours of searching. Python why.
     pack_meta = get_pack_meta(5737).text
-    name_string = """"zh_TW":"""
+    name_string = """"ko":"""
     pack_name = get_pack_name(name_string, pack_meta)
+    p1 = bytes(pack_name, 'utf-8').decode('unicode_escape')
     print(pack_name)
-    p1 = bytes(pack_name, 'utf-32').decode('unicode_escape')
     print(p1)
 
     pack_meta = get_pack_meta(5737).text
